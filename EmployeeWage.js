abb78950ff46b0432852f57558b0a5ea8eeedd67
= <kamalakars04@gmail.com>
2020-11-16 18:24:00 +0530

    [Kamalakar].[Refactor].Adding UC6_StoreDailyWage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,11 +1,11 @@
 // UC 1
 // Checks if employee is present or absent
 const IS_ABSENT=0;
-let empCheck=Math.floor(Math.random()*10)%2;
+let empCheck = 0;
+empCheck=Math.floor(Math.random()*10)%2;
 if(empCheck==IS_ABSENT)
 {
     console.log("Employee is absent");
-    return;
 }
 else
 {
@@ -51,15 +51,16 @@ function GetWorkingHours(empCheck)
             return 0;
     }
 }
+empCheck=Math.floor(Math.random()*10)%3;
 empHrs=GetWorkingHours(empCheck);
-console.log("Employee Wage: "+empHrs);
+console.log("Employee hours: "+empHrs);
 
 // UC 4
 // Calculates wage for a total month
 const NO_OF_WORKING_DAYS=20;
 for(let day=0; day<NO_OF_WORKING_DAYS; day++)
 {
-    let empCheck=Math.floor(Math.random()*10)%3;
+    empCheck=Math.floor(Math.random()*10)%3;
     empHrs+=GetWorkingHours(empCheck);
 }
 empWage=empHrs*WAGE_PER_HOUR;
@@ -67,13 +68,22 @@ console.log("Total Hrs: "+empHrs+" Employee Wage: "+empWage);
 
 // UC 5
 // Calculate employee wage for a maximmum of 100 hours
+let dailyWages = [];
 const MAX_HRS_IN_MONTH=100;
+let dailyEmpHours = 0;
 let totalEmpHrs=0;
 let totalWorkingDays=0;
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS)
 {
     totalWorkingDays++;
-    totalEmpHrs += GetWorkingHours(empCheck);
+    empCheck=Math.floor(Math.random()*10)%3;
+    dailyEmpHours = GetWorkingHours(empCheck);
+    totalEmpHrs += dailyEmpHours;
+    dailyWages.push(dailyEmpHours*WAGE_PER_HOUR);
+
 }
 empWage=totalEmpHrs*WAGE_PER_HOUR;
 console.log("Total No of Days: "+totalWorkingDays+" Total Hrs Worked: "+totalEmpHrs+ "Total Employee Wage: "+empWage);
+
+// UC6 Store Daily Wage
+dailyWages.forEach(p=> console.log("DailyWage on day is "+ p));
